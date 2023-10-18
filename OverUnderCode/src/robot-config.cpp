c78a11249355478d3340d065d710aa9290659ed4
Ctrl-Shift-QQ <124120286+Ctrl-Shift-QQ@users.noreply.github.com>
2023-10-18 16:04:45 -0700

    port :(

--- OverUnderCode/src/robot-config.cpp
+++ OverUnderCode/src/robot-config.cpp
@@ -9,19 +9,19 @@ brain  Brain;
 
 // VEXcode device constructors
 controller Controller1 = controller(primary);
-motor LeftFront = motor(PORT16, ratio6_1, true);
-motor LeftStack = motor(PORT15, ratio6_1, true);
-motor LeftBack = motor(PORT18, ratio6_1, true);
-motor RightFront = motor(PORT1, ratio6_1, false);
-motor RightMiddle = motor(PORT14, ratio6_1, false);
-motor RightBack = motor(PORT13, ratio6_1, false);
+motor LeftFront = motor(PORT1, ratio6_1, true);
+motor LeftStack = motor(PORT2, ratio6_1, true);
+motor LeftBack = motor(PORT3, ratio6_1, true);
+motor RightFront = motor(PORT10, ratio6_1, false);
+motor RightMiddle = motor(PORT9, ratio6_1, false);
+motor RightBack = motor(PORT8, ratio6_1, false);
 motor Intake = motor(PORT17, ratio6_1, true);
 motor Catapult = motor(PORT3, ratio36_1, false);
 inertial Inertial = inertial(PORT11);
 rotation Rotation = rotation(PORT10);
 digital_out Wings = digital_out(Brain.ThreeWirePort.B);
 
-
+ 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
 bool RemoteControlCodeEnabled = true;
