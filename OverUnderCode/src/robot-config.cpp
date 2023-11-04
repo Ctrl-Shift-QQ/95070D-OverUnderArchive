af73e84f528d12d618aaa1b0a3f7a97f5bd6a7c0
Ctrl-Shift-QQ <124120286+Ctrl-Shift-QQ@users.noreply.github.com>
2023-11-04 15:06:40 -0700

    port

--- OverUnderCode/src/robot-config.cpp
+++ OverUnderCode/src/robot-config.cpp
@@ -12,7 +12,7 @@ brain  Brain;
 
 // VEXcode device constructors
 controller Controller1 = controller(primary);
-motor LeftFront = motor(PORT13, ratio6_1, true);
+motor LeftFront = motor(PORT18, ratio6_1, true);
 motor LeftMiddle = motor(PORT20, ratio6_1, true);
 motor LeftBack = motor(PORT8, ratio6_1, true);
 motor RightFront = motor(PORT13, ratio6_1, false);
