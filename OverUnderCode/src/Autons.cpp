b671e2701841414e88df55473f2f3da7c439fd40
Ctrl-Shift-QQ <124120286+Ctrl-Shift-QQ@users.noreply.github.com>
2024-02-17 14:20:38 -0800

    m

--- OverUnderCode/src/Autons.cpp
+++ OverUnderCode/src/Autons.cpp
@@ -6,6 +6,18 @@
 
 void runProgrammingSkills(){
   drive(Reverse, 16);
+  turnTo(250);
+  drive(Forward, 2);
+  Kicker.setVelocity(57, percent); 
+  Kicker.spin(forward);
+  wait(25, sec);
+  Kicker.stop(); //Kicker Fired
+
+  drive(Reverse, 4);
+  turnTo(0);
+  drive(Reverse, 8);
   swingTo(315, Right, Reverse);
-  ram(Reverse, 12);
+  ram(Reverse, 12); //Preloads Scored
+
+  
 }
